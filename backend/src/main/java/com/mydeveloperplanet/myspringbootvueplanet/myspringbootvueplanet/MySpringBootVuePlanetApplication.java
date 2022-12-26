83e881cec3343108ab23cd0794d2474760a97e16
mydeveloperplanet <gunter@mydeveloperplanet.com>
2022-12-26 14:08:20 +0100

    Initial commit

---
+++ backend/src/main/java/com/mydeveloperplanet/myspringbootvueplanet/myspringbootvueplanet/MySpringBootVuePlanetApplication.java
@@ -0,0 +1,13 @@
+package com.mydeveloperplanet.myspringbootvueplanet.myspringbootvueplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootVuePlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootVuePlanetApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mydeveloperplanet/myspringbootvueplanet/myspringbootvueplanet/MySpringBootVuePlanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydeveloperplanet.myspringbootvueplanet.myspringbootvueplanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootVuePlanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
